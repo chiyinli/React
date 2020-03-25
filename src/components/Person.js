2cca366a2ae35d141ae677e5bfde83797058a368
Chi Yin Li <chiyinli.mastercyber1@gmail.com>
2020-03-25 07:33:01 +0000

    My React Projects

---
+++ src/components/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Person = (props) => {
+    return (
+        <div>
+            <h2>Hi name ia {props.name}</h2>
+        </div>
+    )
+}
+
+export default Person;
---
+++ src/components/Pets.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pets = (props) => {
+    return (
+        <div>
+           <h1>My pets name is {props.animals}</h1> 
+        </div>
+    )
+}
+
+export default Pets;
